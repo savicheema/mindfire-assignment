d66a7d28f35ad7391842fec99e9ea83e653e5c69
Savitoj Cheema <savitojc@Savitojs-MacBook-Air.local>
2021-05-18 23:21:46 +0530

    signin page heading copy fix

--- components/SigninForm.tsx
+++ components/SigninForm.tsx
@@ -16,7 +16,7 @@ class SignInForm extends React.Component<SignInFormProps, SignInFormState> {
 
 
 
-        return (<div className={styles.signInForm}><div className={styles.title}>Sign In to MINDFIRE</div>
+        return (<div className={styles.signInForm}><div className={styles.title}>Sign in to MINDFIRE</div>
             {isError && <div className={styles.error}>Passwords do not match!</div>}
             <form className={styles.formGroup}>
 
